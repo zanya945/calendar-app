725dae0924355aed446b158cc6c36a5436a8cac2
zanzan <tea915656@gmail.com>
2021-12-16 20:46:51 +0800

    add input

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
+  const [num, setnum] = useState(' ');
+  let phnum: string = "";
+  function printnum(num: string) {
+    
+    return num;
+  }
   return (
     <View style={styles.container}>
-      <Text>OWO</Text>
+      <Text style={styles.text}>您輸入的號碼是:{printnum(num)}</Text>
+      <TextInput
+        onChangeText={setnum}
+        value={num}
+        style={styles.TextInput}
+        maxLength={13}
+      ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +27,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    fontSize: 32,
+    backgroundColor: '#E9AEAE',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInput: {
+    fontSize: 32,
+    backgroundColor: 'gray',
+    textAlign: 'center',
+    color: 'white',
+    borderColor: 'black',
+  },
+  text: {
+    fontSize: 32,
+
+  }
 });
